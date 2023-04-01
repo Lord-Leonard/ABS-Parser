643c8b691e1a3f7bf4ef0621f66fa3be6b8c04dd
Lord-Leonard <leonardgeorg@gmx.de>
2023-04-01 19:16:26 +0200

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\leoge\RiderProjects\ConsoleApp1\ConsoleApp1\TWPL-5-BW-02-620-A_BFD.abs";
+            
+            var lines = File.ReadLines(fileName);
+            Parallel.ForEach(lines, line =>
+            {
+                string[] fields = line.Split('@');
+
+                Console.WriteLine(fields[0]);
+
+                switch (fields[0])
+                {
+                    case "BF2D":
+                        Console.WriteLine();
+                }
+                
+            });
+        }
+    }
+}
---
+++ ConsoleApp1/Sentence.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    public class Sentence
+    {
+        public string? Projektnummer;
+        public string Plannummer;
+        public string Planindex;
+        public string Position;
+        public string Länge; // Einheit: mm
+        public string Menge; 
+        public string Gewicht; // Einheit: kg
+        public string Durchmesser; // Einheit: mm
+        public string Stahlgüte;
+        public string Biegerollendurchmesser; // Einheit: mm
+        public string Mattentyp;
+        public string Breite; // Einheit: mm
+        public string Prüfziffer;
+
+    }
+}
